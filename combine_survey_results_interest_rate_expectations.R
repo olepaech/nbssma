cb622f9049760be0ebd0fd0ac64622f17c29aacd
Ole Paech <ole.paech@gmx.de>
2025-07-07 10:45:22 +0200

    package built and remove of percent sign

--- combine_survey_results_interest_rate_expectations.R
+++ combine_survey_results_interest_rate_expectations.R
@@ -6,16 +6,18 @@ library(zoo)
 library(stringr)
 
 combine_survey_results <- function(subfolder) {
-  
+
   extract_block <- function(df, start_row, end_row) {
     df_block <- df[(start_row + 1):(end_row - 1), ]
     colnames(df_block) <- c("Date", "DFR", "MRO", "MLF", "€STR", "3-months EURIBOR")
     df_block <- df_block %>% filter(!if_all(everything(), is.na))
+    df_block <- df_block %>%
+      mutate(across(-Date, ~ as.numeric(str_replace_all(as.character(.x), "%|\\s", ""))))
     return(df_block)
   }
-  
+
   parse_participant_file <- function(file) {
-    raw <- raw <- suppressMessages(read_excel(file, col_names = FALSE))
+    raw <- raw <- suppressMessages(read_excel(file, sheet = 1, col_names = FALSE))
     start_gc <- which(raw[[1]] == "GC meetings")
     start_q  <- which(raw[[1]] == "Quarters")
     start_lr <- which(raw[[1]] == "Long run")
@@ -28,14 +30,14 @@ combine_survey_results <- function(subfolder) {
       longrun  = extract_block(raw, start_lr, end_lr)
     )
   }
-  
+
   load_participant_files <- function(subfolder) {
     base_path <- "C:/Users/olepa/OneDrive/Desktop/NBS/Answers"
     full_path <- file.path(base_path, subfolder)
     files <- list.files(full_path, pattern = "\\.xlsx$", full.names = TRUE)
     return(files)
   }
-  
+
   combine_field <- function(section, field) {
     df_list <- map2(all_data, participant_names, function(participant, name) {
       df <- participant[[section]]
@@ -46,14 +48,14 @@ combine_survey_results <- function(subfolder) {
     })
     reduce(df_list, full_join, by = "Date")
   }
-  
+
   files <- load_participant_files(subfolder)
   participant_names <- paste0("Teilnehmer_", seq_along(files))
   all_data <- map(files, parse_participant_file)
-  
+
   sections <- c("gc", "quarters", "longrun")
   fields <- c("DFR", "MRO", "MLF", "€STR", "3-months EURIBOR")
-  
+
   combined_results <- list()
   for (sec in sections) {
     for (field in fields) {
@@ -61,8 +63,7 @@ combine_survey_results <- function(subfolder) {
       combined_results[[paste(sec, field, sep = "_")]] <- df_combined
     }
   }
-  
-  # Date-Feld für GC-Tabellen als yearmon formatieren
+
   gc_names <- grep("^gc_", names(combined_results), value = TRUE)
   for (name in gc_names) {
     combined_results[[name]]$Date <- as.yearmon(as.Date(as.numeric(combined_results[[name]]$Date), origin = "1899-12-30"))
